85eb1b7385c3a2b3ec1294f81e87fef535b5b443
Nick Hoffman <nick@deadorange.com>
2012-04-12 16:41:36 -0400

    Bump to v1.0.1 .

--- spec/bson_object_id_validations_spec.rb
+++ spec/bson_object_id_validations_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe BsonObjectIdValidations do
-  it 'is at version 0.0.1' do
-    BsonObjectIdValidations::VERSION.should == '1.0.0'
+  it 'is at version 1.0.1' do
+    BsonObjectIdValidations::VERSION.should == '1.0.1'
   end
 end
